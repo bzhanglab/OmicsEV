cdadc4e1b17fd1282e94d45816c01533dc5568ba
Bo Wen <wenbostar@gmail.com>
2019-10-10 17:32:58 -0500

    Updated axis labels for complex analysis figure

--- R/metrics.R
+++ R/metrics.R
@@ -271,6 +271,8 @@ plot_network_cor=function(x, out_dir="./",prefix="test"){
     gg <- ggplot(dat,aes(x=nid,y=cor,colour=type))+
         #geom_violin(position="dodge")+
         geom_boxplot(position="dodge")+
+        xlab("Dataset")+
+        ylab("Correlation")+
         theme(axis.text.x = element_text(angle = 90, hjust = 1))
     print(gg)
     dev.off()
