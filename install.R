7923eed6f5f5ecc2fb7339c025f433b1e9c1e6af
Bo <wenbostar@gmail.com>
2020-04-10 00:49:01 -0500

    Update install.R

--- install.R
+++ install.R
@@ -6,5 +6,6 @@ BiocManager::install("theislab/kBET")
 BiocManager::install("wenbostar/metaX")
 BiocManager::install("bzhanglab/NetSAM")
 BiocManager::install("AnalytixWare/ShinySky")
+BiocManager::install("doMC")
 BiocManager::install("bzhanglab/OmicsEV")
 
